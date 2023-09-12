29b508ec5e624989573f438d077a780da615ca3a
kavitha-sundararajan <90255023+kavitha-sundararajan@users.noreply.github.com>
2023-09-12 15:14:48 +0530

    BAH-3209 | added provider info in the obs response (#230)

--- bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniObsResource.java
+++ bahmnicore-omod/src/main/java/org/openmrs/module/bahmnicore/web/v1_0/resource/BahmniObsResource.java
@@ -2,8 +2,11 @@ package org.openmrs.module.bahmnicore.web.v1_0.resource;
 
 
 import org.openmrs.Encounter;
+import org.openmrs.EncounterProvider;
 import org.openmrs.Obs;
 import org.openmrs.Visit;
+import org.openmrs.module.bahmniemrapi.drugorder.mapper.BahmniProviderMapper;
+import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.representation.NamedRepresentation;
@@ -11,7 +14,9 @@ import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_11.ObsResource1_11;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @org.openmrs.module.webservices.rest.web.annotation.Resource(name = RestConstants.VERSION_1 + "/obs", supportedClass = Obs.class, supportedOpenmrsVersions = {"1.10.* - 2.*"}, order = 0)
 public class BahmniObsResource extends ObsResource1_11 {
@@ -29,6 +34,7 @@ public class BahmniObsResource extends ObsResource1_11 {
             description.addProperty("visitUuid");
             description.addProperty("encounterUuid");
             description.addProperty("visitStartDateTime");
+            description.addProperty("providerList");
             return description;
         }
         return representationDescription;
@@ -52,6 +58,16 @@ public class BahmniObsResource extends ObsResource1_11 {
     public String getEncounterUuid(Obs obs) {
         Encounter encounter = obs.getEncounter();
         return encounter.getUuid();
+    }
+
+    @PropertyGetter("providerList")
+    public List<EncounterTransaction.Provider> getProviderList(Obs obs) {
+        List<EncounterTransaction.Provider> providerList = new ArrayList<>();
+        BahmniProviderMapper bahmniProviderMapper = new BahmniProviderMapper();
+        for (EncounterProvider encounterProvider : obs.getEncounter().getEncounterProviders()) {
+            providerList.add(bahmniProviderMapper.map(encounterProvider.getProvider()));
+        }
+        return providerList;
     }
 }
 
